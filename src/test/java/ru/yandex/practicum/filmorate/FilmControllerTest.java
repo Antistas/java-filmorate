705bf3a0a670b4eca3f85b1bb96645062c51fae6
Antistas <xzeitz@yandex.ru>
2026-03-31 15:53:54 +0300

    Добавлены тесты

--- src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
@@ -1,9 +1,18 @@
 package ru.yandex.practicum.filmorate;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.controller.FilmController;
+import ru.yandex.practicum.filmorate.controller.UserController;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.FilmService;
+import ru.yandex.practicum.filmorate.service.UserService;
+import ru.yandex.practicum.filmorate.storage.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.InMemoryFilmStorage;
+import ru.yandex.practicum.filmorate.storage.InMemoryUserStorage;
+import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import java.time.LocalDate;
 
@@ -11,32 +20,61 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class FilmControllerTest {
 
+    private FilmController filmController;
+    private UserController userController;
+
+    @BeforeEach
+    void setUp() {
+        FilmStorage filmStorage = new InMemoryFilmStorage();
+        UserStorage userStorage = new InMemoryUserStorage();
+
+        FilmService filmService = new FilmService(filmStorage, userStorage);
+        UserService userService = new UserService(userStorage);
+
+        filmController = new FilmController(filmStorage, filmService);
+        userController = new UserController(userStorage, userService);
+    }
+
+    private Film createNewFilm() {
+        Film film = new Film();
+        film.setName("Valid film");
+        film.setDescription("Description");
+        film.setDuration(100);
+        film.setReleaseDate(LocalDate.of(2005, 12, 28));
+        return film;
+    }
+
+    private User createNewUser(String email) {
+        User user = new User();
+        user.setEmail(email);
+        user.setLogin("rustam");
+        user.setName("");
+        user.setBirthday(LocalDate.of(1990, 1, 1));
+        return user;
+    }
+
     // фильм до даты рождения
     @Test
     void shouldThrowExceptionWhenReleaseDateBeforeCinemaBirthday() {
-        FilmController controller = new FilmController();
-
         Film film = new Film();
         film.setName("Old film");
         film.setDescription("Description");
         film.setDuration(100);
         film.setReleaseDate(LocalDate.of(1895, 12, 27));
 
-        assertThrows(ValidationException.class, () -> controller.postFilm(film));
+        assertThrows(ValidationException.class, () -> filmController.postFilm(film));
     }
 
     // фильм в дату рождения
     @Test
     void shouldCreateFilmWhenReleaseDateIsCinemaBirthday() {
-        FilmController controller = new FilmController();
-
         Film film = new Film();
         film.setName("Valid film");
         film.setDescription("Description");
         film.setDuration(100);
         film.setReleaseDate(LocalDate.of(1895, 12, 28));
 
-        Film createdFilm = controller.postFilm(film);
+        Film createdFilm = filmController.postFilm(film);
 
         assertNotNull(createdFilm.getId());
         assertEquals("Valid film", createdFilm.getName());
@@ -45,14 +83,47 @@ public class FilmControllerTest {
     // фильм с неизвестным ID
     @Test
     void shouldThrowExceptionWhenUpdatingUnknownFilm() {
-        FilmController controller = new FilmController();
         Film film = new Film();
         film.setId(666L);
         film.setName("Unknown Film");
         film.setDescription("desc");
         film.setDuration(120);
         film.setReleaseDate(LocalDate.of(2000, 1, 1));
-        assertThrows(RuntimeException.class, () -> controller.updateFilm(film));
+        assertThrows(RuntimeException.class, () -> filmController.updateFilm(film));
+    }
+
+    @Test
+    void shouldAddLike() {
+        Film createdFilm = filmController.postFilm(createNewFilm());
+        User createdUser = userController.postUser(createNewUser("rustam@yandex.ru"));
+
+        filmController.addLike(createdFilm.getId(), createdUser.getId());
+        assertEquals(1, createdFilm.getLikes().size());
+    }
+
+    @Test
+    void shouldRemoveLike() {
+        Film createdFilm = filmController.postFilm(createNewFilm());
+        User createdUser = userController.postUser(createNewUser("rustam@yandex.ru"));
+        User createdUser2 = userController.postUser(createNewUser("rustam2@yandex.ru"));
+
+        filmController.addLike(createdFilm.getId(), createdUser.getId());
+        filmController.addLike(createdFilm.getId(), createdUser2.getId());
+        filmController.removeLike(createdFilm.getId(), createdUser.getId());
+        assertEquals(1, createdFilm.getLikes().size());
+        assertTrue(createdFilm.getLikes().contains(createdUser2.getId()));
+    }
+
+    @Test
+    void shouldThrownExceptionWhenCountIsNegative() {
+        filmController.postFilm(createNewFilm());
+        assertThrows(IllegalArgumentException.class, () -> filmController.getPopularFilms(-1));
+    }
+
+    @Test
+    void shouldThrownExceptionWhenCountIsZero() {
+        filmController.postFilm(createNewFilm());
+        assertThrows(IllegalArgumentException.class, () -> filmController.getPopularFilms(0));
     }
 
 }
--- src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
@@ -1,38 +1,51 @@
 package ru.yandex.practicum.filmorate;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.controller.FilmController;
 import ru.yandex.practicum.filmorate.controller.UserController;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.FilmService;
 import ru.yandex.practicum.filmorate.service.UserService;
+import ru.yandex.practicum.filmorate.storage.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.InMemoryFilmStorage;
 import ru.yandex.practicum.filmorate.storage.InMemoryUserStorage;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserControllerTest {
 
-    private UserController createController() {
+    private FilmController filmController;
+    private UserController userController;
+
+    @BeforeEach
+    void setUp() {
+        FilmStorage filmStorage = new InMemoryFilmStorage();
         UserStorage userStorage = new InMemoryUserStorage();
+
+        FilmService filmService = new FilmService(filmStorage, userStorage);
         UserService userService = new UserService(userStorage);
-        return new UserController(userStorage, userService);
+
+        filmController = new FilmController(filmStorage, filmService);
+        userController = new UserController(userStorage, userService);
     }
 
     // пользователь добавляется с пустым именем
     @Test
     void shouldSetLoginAsNameWhenNameIsBlank() {
-        UserController controller = createController();
-
         User user = new User();
         user.setEmail("rustam@mail.com");
         user.setLogin("rustam");
         user.setName("");
         user.setBirthday(LocalDate.of(1990, 1, 1));
 
-        User createdUser = controller.postUser(user);
+        User createdUser = userController.postUser(user);
 
         assertNotNull(createdUser.getId());
         assertEquals("rustam", createdUser.getName());
@@ -41,14 +54,13 @@ class UserControllerTest {
     // исключение при добавлении пользователя с таким же емейлом
     @Test
     void shouldThrowExceptionWhenEmailAlreadyExistsOnCreate() {
-        UserController controller = createController();
 
         User firstUser = new User();
         firstUser.setEmail("rustam@mail.com");
         firstUser.setLogin("rustam");
         firstUser.setName("Rustam");
         firstUser.setBirthday(LocalDate.of(1990, 1, 1));
-        controller.postUser(firstUser);
+        userController.postUser(firstUser);
 
         User secondUser = new User();
         secondUser.setEmail("rustam@mail.com");
@@ -56,14 +68,12 @@ class UserControllerTest {
         secondUser.setName("Rustam 2");
         secondUser.setBirthday(LocalDate.of(1992, 2, 2));
 
-        assertThrows(ValidationException.class, () -> controller.postUser(secondUser));
+        assertThrows(ValidationException.class, () -> userController.postUser(secondUser));
     }
 
     // обновляем несуществующего пользователя
     @Test
     void shouldThrowExceptionWhenUpdatingUnknownUser() {
-        UserController controller = createController();
-
         User user = new User();
         user.setId(666L);
         user.setEmail("rustam@mail.com");
@@ -71,31 +81,143 @@ class UserControllerTest {
         user.setName("Rustam");
         user.setBirthday(LocalDate.of(1990, 1, 1));
 
-        assertThrows(NotFoundException.class, () -> controller.updateUser(user));
+        assertThrows(NotFoundException.class, () -> userController.updateUser(user));
     }
 
     // второй пользователь вписывает существующий емейл
     @Test
     void shouldThrowExceptionWhenUpdatingUserWithDuplicateEmail() {
-        UserController controller = createController();
-
         User firstUser = new User();
         firstUser.setEmail("first@mail.com");
         firstUser.setLogin("first");
         firstUser.setName("First");
         firstUser.setBirthday(LocalDate.of(1990, 1, 1));
-        User createdFirstUser = controller.postUser(firstUser);
+        User createdFirstUser = userController.postUser(firstUser);
 
         User secondUser = new User();
         secondUser.setEmail("second@mail.com");
         secondUser.setLogin("second");
         secondUser.setName("Second");
         secondUser.setBirthday(LocalDate.of(1992, 2, 2));
-        User createdSecondUser = controller.postUser(secondUser);
+        User createdSecondUser = userController.postUser(secondUser);
 
         createdSecondUser.setEmail(createdFirstUser.getEmail());
 
-        assertThrows(ValidationException.class, () -> controller.updateUser(createdSecondUser));
+        assertThrows(ValidationException.class, () -> userController.updateUser(createdSecondUser));
+    }
+
+    @Test
+    void shouldAddFriend() {
+        User firstUser = new User();
+        firstUser.setEmail("first@mail.com");
+        firstUser.setLogin("first");
+        firstUser.setName("First");
+        firstUser.setBirthday(LocalDate.of(1990, 1, 1));
+        User createdFirstUser = userController.postUser(firstUser);
+
+        User secondUser = new User();
+        secondUser.setEmail("second@mail.com");
+        secondUser.setLogin("second");
+        secondUser.setName("Second");
+        secondUser.setBirthday(LocalDate.of(1992, 2, 2));
+        User createdSecondUser = userController.postUser(secondUser);
+
+        userController.addFriend(createdFirstUser.getId(), createdSecondUser.getId());
+        assertEquals(1, createdFirstUser.getFriends().size());
+        assertEquals(1, createdSecondUser.getFriends().size());
+        assertTrue(createdSecondUser.getFriends().contains(createdFirstUser.getId()));
+        assertTrue(createdFirstUser.getFriends().contains(createdSecondUser.getId()));
+    }
+
+    @Test
+    void shouldRemoveFriend() {
+        User firstUser = new User();
+        firstUser.setEmail("first@mail.com");
+        firstUser.setLogin("first");
+        firstUser.setName("First");
+        firstUser.setBirthday(LocalDate.of(1990, 1, 1));
+        User createdFirstUser = userController.postUser(firstUser);
+
+        User secondUser = new User();
+        secondUser.setEmail("second@mail.com");
+        secondUser.setLogin("second");
+        secondUser.setName("Second");
+        secondUser.setBirthday(LocalDate.of(1992, 2, 2));
+        User createdSecondUser = userController.postUser(secondUser);
+        userController.addFriend(createdFirstUser.getId(), createdSecondUser.getId());
+        userController.removeFriend(createdFirstUser.getId(), createdSecondUser.getId());
+
+
+        assertEquals(0, createdFirstUser.getFriends().size());
+        assertEquals(0, createdSecondUser.getFriends().size());
+        assertFalse(createdSecondUser.getFriends().contains(createdFirstUser.getId()));
+        assertFalse(createdFirstUser.getFriends().contains(createdSecondUser.getId()));
+    }
+
+    @Test
+    void shouldReturnListOfCommonFriends() {
+            User firstUser = new User();
+            firstUser.setEmail("first@mail.com");
+            firstUser.setLogin("first");
+            firstUser.setName("First");
+            firstUser.setBirthday(LocalDate.of(1990, 1, 1));
+            User createdFirstUser = userController.postUser(firstUser);
+
+            User secondUser = new User();
+            secondUser.setEmail("second@mail.com");
+            secondUser.setLogin("second");
+            secondUser.setName("Second");
+            secondUser.setBirthday(LocalDate.of(1992, 2, 2));
+            User createdSecondUser = userController.postUser(secondUser);
+
+            User thirdUser = new User();
+            thirdUser.setEmail("third@mail.com");
+            thirdUser.setLogin("third");
+            thirdUser.setName("third");
+            thirdUser.setBirthday(LocalDate.of(1992, 2, 2));
+            User createdThirdUser = userController.postUser(thirdUser);
+
+            userController.addFriend(createdFirstUser.getId(), createdSecondUser.getId());
+            userController.addFriend(createdThirdUser.getId(), createdSecondUser.getId());
+
+            assertEquals(List.of(createdSecondUser),
+                    userController.getCommonFriends(createdFirstUser.getId(), createdThirdUser.getId()));
+    }
+
+    @Test
+    void shouldReturnEmptyListOfCommonFriends() {
+        User firstUser = new User();
+        firstUser.setEmail("first@mail.com");
+        firstUser.setLogin("first");
+        firstUser.setName("First");
+        firstUser.setBirthday(LocalDate.of(1990, 1, 1));
+        User createdFirstUser = userController.postUser(firstUser);
+
+        User secondUser = new User();
+        secondUser.setEmail("second@mail.com");
+        secondUser.setLogin("second");
+        secondUser.setName("Second");
+        secondUser.setBirthday(LocalDate.of(1992, 2, 2));
+        User createdSecondUser = userController.postUser(secondUser);
+
+        User thirdUser = new User();
+        thirdUser.setEmail("third@mail.com");
+        thirdUser.setLogin("third");
+        thirdUser.setName("third");
+        thirdUser.setBirthday(LocalDate.of(1992, 2, 2));
+        User createdThirdUser = userController.postUser(thirdUser);
+
+        User fourthUser = new User();
+        fourthUser.setEmail("fourth@mail.com");
+        fourthUser.setLogin("fourth");
+        fourthUser.setName("fourth");
+        fourthUser.setBirthday(LocalDate.of(1992, 2, 2));
+        User createdFourthUser = userController.postUser(fourthUser);
+
+        userController.addFriend(createdFirstUser.getId(), createdSecondUser.getId());
+        userController.addFriend(createdThirdUser.getId(), createdFourthUser.getId());
+
+        assertEquals(List.of(),userController.getCommonFriends(createdFirstUser.getId(), createdThirdUser.getId()));
     }
 
 }
