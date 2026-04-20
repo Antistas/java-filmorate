45d9612e3ed02a7d3a50b91ffc06c011e3e420f9
Antistas <xzeitz@yandex.ru>
2026-04-20 22:15:03 +0300

    Обновлены sql

--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO mpa_rating (id, name, comment) VALUES (1, 'G', 'Нет возрастных ограничений');
-INSERT INTO mpa_rating (id, name, comment) VALUES (2, 'PG', 'Рекомендуется смотреть с родителями');
