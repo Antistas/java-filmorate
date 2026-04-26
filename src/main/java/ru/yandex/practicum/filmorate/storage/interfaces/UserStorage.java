cf081e26f3460b70192f8300b5fe42fdb91542bf
Antistas <xzeitz@yandex.ru>
2026-04-26 18:07:47 +0300

    чекстайл

--- src/main/java/ru/yandex/practicum/filmorate/storage/interfaces/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/interfaces/UserStorage.java
@@ -1,10 +1,7 @@
 package ru.yandex.practicum.filmorate.storage.interfaces;
 
-import ru.yandex.practicum.filmorate.model.FriendshipStatus;
 import ru.yandex.practicum.filmorate.model.User;
-
 import java.util.Collection;
-import java.util.List;
 import java.util.Optional;
 
 public interface UserStorage {
