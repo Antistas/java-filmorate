284728fa6e8f78d9adf1c70023010dc984571241
Antistas <xzeitz@yandex.ru>
2026-04-28 09:06:34 +0300

    Исправления согласно ревью 1) убраны лишние зависимости 2) изменено условие

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -6,8 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
-import ru.yandex.practicum.filmorate.service.GenreService;
-import ru.yandex.practicum.filmorate.service.MpaService;
 
 import java.util.Collection;
 import java.util.List;
